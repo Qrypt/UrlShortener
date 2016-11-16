52079ddd53a6fccd3d58f5ad2420c47e227df510
Fredrik Berg <fr3drik.berg@gmail.com>
2016-11-16 16:05:36 +0100

    Google urlShortener is now working properly, can however not display result as link i view-table

--- src/main/java/qrypt/work/MyUrlController.java
+++ src/main/java/qrypt/work/MyUrlController.java
@@ -4,28 +4,39 @@ package qrypt.work;
 Google api fuckar upp tomcat av någon anledning...
  */
 
-//import com.google.api.client.extensions.appengine.http.UrlFetchTransport;
-//import com.google.api.client.googleapis.extensions.appengine.auth.oauth2.AppIdentityCredential;
-//import com.google.api.client.json.jackson2.JacksonFactory;
-//import com.google.api.services.urlshortener.Urlshortener;
-//import com.google.api.services.urlshortener.UrlshortenerScopes;
-//import com.google.api.services.urlshortener.model.Url;
-//import com.google.api.services.urlshortener.model.UrlHistory;
+import com.google.api.client.extensions.appengine.http.UrlFetchTransport;
+import com.google.api.client.googleapis.extensions.appengine.auth.oauth2.AppIdentityCredential;
+import com.google.api.client.googleapis.services.CommonGoogleClientRequestInitializer;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.urlshortener.Urlshortener;
+import com.google.api.services.urlshortener.UrlshortenerScopes;
 
-import org.omg.PortableServer.LIFESPAN_POLICY_ID;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 @Controller
 @RequestMapping(value = "/myurl")
 public class MyUrlController {
+    /*
+    Connect url för förkortning av url:
+
+    https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyDnLjS_ypgz3omGtT3VKRBYGZKNl0XIf3E
+     */
+
     private final String GOOGLE_API_KEY = "AIzaSyDnLjS_ypgz3omGtT3VKRBYGZKNl0XIf3E";
 
     private MyUrlRepository myUrlRepository;
@@ -49,12 +60,55 @@ public class MyUrlController {
     public String create( MyUrl myUrl) {
 
         MyUrl tmpUrl = myUrl;
-        tmpUrl.setShortened("www.short-version.com");
+
+        try {
+            tmpUrl.setShortened(getShortUrl(tmpUrl.getOriginal()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         myUrlRepository.save(tmpUrl);
         return "redirect:/myurl";
     }
 
+    private String getShortUrl(String longUrl) throws IOException {
+        URL url = new URL("https://www.googleapis.com/urlshortener/v1/url?key=" + GOOGLE_API_KEY);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+        con.setDoInput(true);
+        con.setDoOutput(true);
+        con.setRequestProperty("Content-type", "application/json");
+        con.setRequestProperty("Accept", "application/json");
+        con.setRequestMethod("POST");
+
+        JSONObject urlData = new JSONObject();
+
+        urlData.put("longUrl", longUrl);
+
+        OutputStreamWriter writer =  new OutputStreamWriter(con.getOutputStream());
+        writer.write(urlData.toString());
+        writer.flush();
+
+
+        StringBuilder sb = new StringBuilder();
+        int HttpResult = con.getResponseCode();
+        if(HttpResult == HttpURLConnection.HTTP_OK) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"));
+
+            String line;
+            while((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+            reader.close();
+        }
+
+        JSONObject urlResponse = new JSONObject(sb.toString());
+        String shortUrl = urlResponse.get("id").toString();
+
+        System.out.println("SHORT URL: " + shortUrl);
+
+        return shortUrl;
+    }
 
 //    @RequestMapping(value = "/urlshortener", method = RequestMethod.GET)
 //    public String myUrlForm(Model model) {
@@ -84,24 +138,4 @@ public class MyUrlController {
 //        return "urlshortener";
 //    }
 //
-//    private Urlshortener newUrlshortener() {
-//        AppIdentityCredential credential =
-//                new AppIdentityCredential(Arrays.asList(UrlshortenerScopes.URLSHORTENER));
-//        return new Urlshortener.Builder(new UrlFetchTransport(), new JacksonFactory(), credential)
-//                .build();
-//    }
-//
-//    private ArrayList<String> test(String longUrl) throws IOException {
-//        Urlshortener shortener = newUrlshortener();
-//        UrlHistory history = shortener.url().list().execute();
-//        ArrayList<String> shortUrls = new ArrayList<>();
-//
-//        if (history.getItems() != null) {
-//            for (Url oneShortened : history.getItems()) {
-//                String shorter = oneShortened.getId();
-//                shortUrls.add(shorter);
-//            }
-//        }
-//        return shortUrls;
-//    }
 }
