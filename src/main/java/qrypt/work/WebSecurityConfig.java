4a946bca4c2d8606a9298b5b391ec99669c8eb9a
Fredrik Berg <fr3drik.berg@gmail.com>
2016-11-17 15:13:18 +0100

    changed datasource to get values from application.properties

--- src/main/java/qrypt/work/WebSecurityConfig.java
+++ src/main/java/qrypt/work/WebSecurityConfig.java
@@ -1,39 +1,18 @@
 package qrypt.work;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import javax.sql.DataSource;
-
-
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-
-    private DataSource authDBDataSource;
-
-    @Bean(name = "dataSource")
-    public DriverManagerDataSource dataSource() {
-        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-        driverManagerDataSource.setDriverClassName("org.postgresql.Driver");
-        driverManagerDataSource.setUrl("jdbc:postgresql://localhost:5432/springboot");
-        driverManagerDataSource.setUsername("postgres");
-        driverManagerDataSource.setPassword("postgres123");
-        return driverManagerDataSource;
-    }
-
     @Autowired
-    public void setDatasource(DataSource datasource) {
-        authDBDataSource = datasource;
-    }
+    DatabaseConfig dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -50,10 +29,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll();
     }
 
-
     @Autowired
     public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication().dataSource(dataSource())
+        auth.jdbcAuthentication().dataSource(dataSource.dataSource())
                 .usersByUsernameQuery("select username, password, 'TRUE' from account where username=?")
                 //Dummy query...Har inga roller.
                 .authoritiesByUsernameQuery("select username, 'ROLE_USER' from account where username=?");
